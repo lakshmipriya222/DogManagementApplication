3a32521ad513701a63675fc48a39d99b3ad53aeb
lakshmipriya222 <143149996+lakshmipriya222@users.noreply.github.com>
2024-04-17 00:28:19 -0500

    first commit

---
+++ src/main/java/com/example/DMS/Models/Dog.java
@@ -0,0 +1,43 @@
+package com.example.DMS.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Dog {
+	@Id
+	private int id;
+	private String name;
+	private String breed;
+	@ManyToOne
+	private Trainer trainer;
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getBreed() {
+		return breed;
+	}
+	public Trainer getTrainer() {
+		return trainer;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+	public void setTrainer(Trainer trainer) {
+		this.trainer = trainer;
+	}
+	@Override
+	public String toString() {
+		return "Dog [id=" + id + ", name=" + name + ", breed=" + breed + ", trainer=" + trainer + "]";
+	}
+}
